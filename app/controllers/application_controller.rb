20caf4d838b1b926fdd4f5d1e3fff19d588f692b
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-03-01 00:05:11 +0000

    adds session code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
-    # TODO: @current_user = ...
+    if session["user_id"] #if logged in 
+      @current_user = User.find(session["user_id"])
+    end 
   end
+  
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,25 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # TODO: authenticate user
+    entered_email = params["email"]
+    entered_password = params["password"]
+
+    #check if there is user with that email in database
+    @user = User.find_by({email: entered_email})
+
+    #now, if email matches, check the passowrd
+    if @user 
+      if BCrypt::Password.new(@user.password) == entered_password
+        session["user_id"] = @user.id 
+        flash[:notice] = "Welcome!"
+        redirect_to "/posts"
+      else 
+        flash[:notice] = "Password is incorrect."
+        redirect_to "/sessions/new"
+      end 
+    else 
+      flash[:notice] = "No user with that email address."
+      redirect_to "/sessions/new"
+    end 
   end
 end
