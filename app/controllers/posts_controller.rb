c12d7987c17ca0031ac711b3f6b624df96cd471f
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-03-01 00:20:36 +0000

    adds posts code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   
     def create
       @post = Post.new(params["post"])
+      @post.user_id = @current_user.id 
       @post.save
       redirect_to "/posts"
     end
